55974c2e7efcffd8a5aa325e5c40c0387ed1cf14
Adrien Meyrand <146945051+adrienm94@users.noreply.github.com>
2024-02-13 17:17:36 +0100

    Delete back directory

--- back/src/main/java/com/stage/newRAH/repository/TypeDefautRepository.java
+++
@@ -1,9 +0,0 @@
-package com.stage.newRAH.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.stage.newRAH.model.TypeDefaut;
-
-public interface TypeDefautRepository extends CrudRepository<TypeDefaut, Integer> {
-
-}
--- back/src/main/java/com/stage/newRAH/repository/TypeProjetRepository.java
+++
@@ -1,9 +0,0 @@
-package com.stage.newRAH.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.stage.newRAH.model.TypeProjet;
-
-public interface TypeProjetRepository extends CrudRepository<TypeProjet, Integer> {
-
-}
