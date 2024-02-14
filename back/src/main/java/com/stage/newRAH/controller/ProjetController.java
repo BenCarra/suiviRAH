db07b8e8d6ede7b8551554b2e1cf97bde237f377
Ben Carra <benedictecarra@gmail.com>
2024-02-14 10:20:30 +0100

    Ajout de la méthode createProjet dans le service et le controller

--- back/src/main/java/com/stage/newRAH/controller/ProjetController.java
+++ back/src/main/java/com/stage/newRAH/controller/ProjetController.java
@@ -6,9 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.stage.newRAH.dto.ProjetDTO;
+import com.stage.newRAH.model.Projet;
 import com.stage.newRAH.service.ProjetService;
 
 @RestController
@@ -31,6 +34,11 @@ public class ProjetController {
 	@GetMapping("/projetsByUtilisateur/{id}")
 	public ResponseEntity<List<ProjetDTO>> getProjetsByUtilisateur(@PathVariable int id) {
 		return projetService.getProjetsByUtilisateur(id);
+	}
+
+	@PostMapping("/createProjet")
+	public ResponseEntity<ProjetDTO> createProjet(@RequestBody ProjetDTO request ) {
+		return projetService.createProjet(request);
 	}
 	
 
--- back/src/main/java/com/stage/newRAH/service/ProjetService.java
+++ back/src/main/java/com/stage/newRAH/service/ProjetService.java
@@ -9,14 +9,24 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.stage.newRAH.dto.ProjetDTO;
+import com.stage.newRAH.model.Client;
 import com.stage.newRAH.model.Composition;
 import com.stage.newRAH.model.Equipe;
+import com.stage.newRAH.model.Etat;
 import com.stage.newRAH.model.Projet;
+import com.stage.newRAH.model.TypeDefaut;
+import com.stage.newRAH.model.TypeProjet;
 import com.stage.newRAH.model.Utilisateur;
+import com.stage.newRAH.repository.ClientRepository;
 import com.stage.newRAH.repository.CompositionRepository;
 import com.stage.newRAH.repository.EquipeRepository;
+import com.stage.newRAH.repository.EtatRepository;
 import com.stage.newRAH.repository.ProjetRepository;
+import com.stage.newRAH.repository.TypeDefautRepository;
+import com.stage.newRAH.repository.TypeProjetRepository;
 import com.stage.newRAH.repository.UtilisateurRepository;
+
+
 
 @Service
 public class ProjetService {
@@ -32,6 +42,18 @@ public class ProjetService {
 	
 	@Autowired
 	UtilisateurRepository utilisateurRepository;
+
+	@Autowired
+	ClientRepository clientRepository;
+
+	@Autowired
+	TypeProjetRepository typeProjetRepository;
+
+	@Autowired
+	TypeDefautRepository typeDefautRepository;
+
+	@Autowired
+	EtatRepository etatRepository;
 		
 	public ProjetDTO mapProjetToDTO (Projet projet) {
 		
@@ -122,20 +144,48 @@ public class ProjetService {
 		}
 		return ResponseEntity.ok(projetsDTO);		
 	}
-	
-	
+
+	public ResponseEntity<ProjetDTO> createProjet(ProjetDTO projetDTO) {
+		Projet nouveauProjet = new Projet();
+
+		// Récupération de certains objets par leurs identifiants 
+		Client client = clientRepository.findById(projetDTO.getIdClient()).orElse(null);
+		Etat etat = etatRepository.findById(projetDTO.getIdEtat()).orElse(null);
+		TypeDefaut typeDefaut = typeDefautRepository.findById(projetDTO.getIdTypeDefaut()).orElse(null);
+		TypeProjet typeProjet = typeProjetRepository.findById(projetDTO.getIdTypeProjet()).orElse(null);
+
+		nouveauProjet.setNomProjet(projetDTO.getNomProjet());
+		nouveauProjet.setJira(projetDTO.getJira());
+		nouveauProjet.setTechno(projetDTO.getTechno());
+		nouveauProjet.setService(projetDTO.getService());
+		nouveauProjet.setDateDemande(projetDTO.getDateDemande());
+		nouveauProjet.setLivraisonSouhaitee(projetDTO.getLivraisonSouhaitee());
+		nouveauProjet.setLivraisonRevisee(projetDTO.getLivraisonRevisee());
+		nouveauProjet.setAffectationCDS(projetDTO.getAffectationCDS());
+		nouveauProjet.setPriseEnCompteCDS(projetDTO.getPriseEnCompteCDS());
+		nouveauProjet.setDateEstimation(projetDTO.getDateEstimation());
+		nouveauProjet.setDevisEstimation(projetDTO.getDevisEstimation());
+		nouveauProjet.setDontGarantie(projetDTO.getDontGarantie());
+		nouveauProjet.setDateFeuVert(projetDTO.getDateFeuVert());
+		nouveauProjet.setDateLivraison(projetDTO.getDateLivraison());
+		nouveauProjet.setMCO(projetDTO.isMCO());
+		nouveauProjet.setDatePassageMCO(projetDTO.getDatePassageMCO());
+		nouveauProjet.setDateSortieMCO(projetDTO.getDateSortieMCO());
+		nouveauProjet.setCommentaires(projetDTO.getCommentaires());
+		nouveauProjet.setClient(client);
+		nouveauProjet.setEtat(etat);
+		nouveauProjet.setTypeDefaut(typeDefaut);
+		nouveauProjet.setTypeProjet(typeProjet);
+		
+		// Enregistrement du nouveau projet dans la base de données
+		Projet projetSauvergarde = projetRepository.save(nouveauProjet);
+
+		ProjetDTO projetSauvegardeDTO = mapProjetToDTO(projetSauvergarde);
+
+		return ResponseEntity.ok(projetSauvegardeDTO);
+	}
 }
 	
-//	public ResponseEntity<List<ProjetDTO>> getProjetsByIdComposition(int id) {
-//		Optional<Composition> composition = compositionRepository.findById(id);
-//		Iterable<Projet> projets = projetRepository.findProjetsByComposition(composition);
-//		List<ProjetDTO> projetsDTO = new ArrayList<>();
-//		
-//		for(Projet projet : projets) {
-//			ProjetDTO projetDTO = mapProjetToDTO(projet);			
-//			projetsDTO.add(projetDTO);
-//			}
-//		return ResponseEntity.ok(projetsDTO);
-//		}	
+		
 
 
