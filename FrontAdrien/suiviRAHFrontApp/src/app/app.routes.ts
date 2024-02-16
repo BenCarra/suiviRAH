6c9beda4f078b636da0bf2704186725a23417593
Adrien Meyrand <adrien.meyrand@gmail.com>
2024-02-16 17:51:21 +0100

    Page accueil admin réalisée + test routage

--- FrontAdrien/suiviRAHFrontApp/src/app/app.routes.ts
+++ FrontAdrien/suiviRAHFrontApp/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { PageAccueilAdminComponent } from './page-accueil-admin/page-accueil-admin.component';
+import { HeaderComponent } from './header/header.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{path:'accueil', component:PageAccueilAdminComponent},
+{path:'calendrier', component: HeaderComponent}];
