44170a1cd42827d83c69a24940a350470b4dbb52
Adrien Meyrand <adrien.meyrand@gmail.com>
2024-02-19 10:40:59 +0100

    Ajout autorisation requêtes CORS sur contrôleurs

--- BackAdrien/src/main/java/com/stage/newRAH/controller/TypeTacheController.java
+++ BackAdrien/src/main/java/com/stage/newRAH/controller/TypeTacheController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ import com.stage.newRAH.dto.TypeTacheDTO;
 import com.stage.newRAH.service.TypeTacheService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class TypeTacheController {
 
 	@Autowired
