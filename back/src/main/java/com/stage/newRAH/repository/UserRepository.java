0fc14eba17eb3de0dc534643418d5fcb6b09db08
Ben Carra <benedictecarra@gmail.com>
2024-02-20 16:58:49 +0100

    Suppression de mon front test

--- back/src/main/java/com/stage/newRAH/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.stage.newRAH.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.stage.newRAH.model.User;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-
-}
