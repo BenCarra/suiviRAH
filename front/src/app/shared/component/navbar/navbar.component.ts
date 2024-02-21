ea90f75c18f58a2701d4882da681f279e085044c
Ben Carra <benedictecarra@gmail.com>
2024-02-21 17:18:20 +0100

    Installation de material Angular et création de la navbar

--- front/src/app/shared/component/navbar/navbar.component.ts
+++ front/src/app/shared/component/navbar/navbar.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   // import RouterLink obligatoire pour naviguer sur la navbar
-  imports: [RouterLink], 
+  imports: [RouterLink, MatToolbarModule ], 
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
