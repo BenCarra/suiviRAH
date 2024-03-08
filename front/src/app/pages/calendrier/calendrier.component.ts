d0c5321f81438dfc48eea4958759fb79efde7208
Ben Carra <benedictecarra@gmail.com>
2024-03-08 09:56:00 +0100

    Date du jour, n° de semaine, week-ends et jours fériés d'une autre couleur

--- front/src/app/pages/calendrier/calendrier.component.ts
+++ front/src/app/pages/calendrier/calendrier.component.ts
@@ -13,7 +13,8 @@ import { Utilisateur } from '../../shared/model/utilisateur';
 })
 export class CalendrierComponent implements OnInit {
 
-  currentMonth: Date;
+  today: Date;
+  currentDate: Date;
   daysInMonth: (Date | null)[];
   weekDays: string[] = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
   weeks: { weekNumber: number, days: (Date | null)[] }[] = [];
@@ -25,24 +26,27 @@ export class CalendrierComponent implements OnInit {
   // pour éviter des erreurs à la compilation (le programme essayait d'accéder
   // à prenomUtilisateur alors que l'utilisateur n'était pas encore chargé)
   utilisateur: Utilisateur | any = {};
-
   constructor(private utilisateurService: UtilisateurService) {
-    this.currentMonth = new Date();
+    this.today = new Date();
+    this.currentDate = new Date();
     this.daysInMonth = [];
   }
 
   ngOnInit(): void {
+    
     this.calculateDaysInMonth();
     // Le code ci-dessous permet d'afficher l'utilisateur connecté
     this.utilisateurService.getUtilisateurById(this.idUtilisateurConnecté).subscribe(data => {
            this.utilisateur = data});
+
+    console.log("Current Week Number: ", this.getWeekNumber(this.today));
   }
 
   // Calcule le  nombre de jours dans un mois et cale le 1er jour du mois
   // sous le bon nom de jour grâce aux espaces vides
   calculateDaysInMonth(): void {
-    const year = this.currentMonth.getFullYear();
-    const month = this.currentMonth.getMonth();
+    const year = this.currentDate.getFullYear();
+    const month = this.currentDate.getMonth();
     // daysInMonth renvoie le 0ème jour du mois + 1, ie le dernier
     // jour du mois (et donc le nombre de jours !)
     const daysInMonth = new Date(year, month + 1, 0).getDate();
@@ -103,12 +107,12 @@ export class CalendrierComponent implements OnInit {
   }
   
   goToPreviousMonth(): void {
-    this.currentMonth = new Date(this.currentMonth.getFullYear(), this.currentMonth.getMonth() - 1, 1);
+    this.currentDate = new Date(this.currentDate.getFullYear(), this.currentDate.getMonth() - 1, 1);
     this.calculateDaysInMonth();
   }
 
   goToNextMonth(): void {
-    this.currentMonth = new Date(this.currentMonth.getFullYear(), this.currentMonth.getMonth() + 1, 1);
+    this.currentDate = new Date(this.currentDate.getFullYear(), this.currentDate.getMonth() + 1, 1);
     this.calculateDaysInMonth();
   }
 
@@ -124,6 +128,59 @@ export class CalendrierComponent implements OnInit {
     // Math.ceil permet d'arrondir au dessus
     const weekNo = Math.ceil(( (d.getTime() - yearStart.getTime()) / 86400000 + 1)/7);
     return weekNo;
+  }
+
+  paques(year: number): Date {
+    const a = year % 19;
+    const century = Math.floor(year / 100);
+    const yearsAfterCentury = year % 100;
+    const d =
+    (19 * a + century - Math.floor(century / 4) - Math.floor((Math.floor(century - (century + 8) / 25) + 1) / 3) + 15) %
+    30;
+    const e = (32 + 2 * (century % 4) + 2 * Math.floor(yearsAfterCentury / 4) - d - (yearsAfterCentury % 4)) % 7;
+    const f = d + e - 7 * Math.floor((a + 11 * d + 22 * e) / 451) + 114;
+    const month = Math.floor(f / 31);
+    const day = (f % 31) + 1;
+
+    return new Date(year, month - 1, day);
+  }
+
+  getHolidays(year: number): Date[] {
+    const easter = this.paques(year);
+
+    // lundi de Pâques
+    const easterMonday= new Date (easter.getFullYear(), easter.getMonth(), easter.getDate()+1)
+    // Ascension
+    const ascension = new Date (easter.getFullYear(), easter.getMonth(), easter.getDate()+39);
+    
+    const holidays: Date[] = [
+     new Date(year, 0, 1), // Jour de l'An
+     easterMonday,
+     new Date(year, 4, 1), // Fête du travail
+     new Date(year, 4, 8), // Victoire 1945
+     ascension,
+     new Date(year, 6, 14), // Fête Nationale
+     new Date(year, 7, 15), // Assomption
+     new Date(year, 10, 1), // Toussaint
+     new Date(year, 10, 11), // Armistice 1918
+     new Date(year, 11, 25), // Noël
+    ]
+    return holidays;
+  }
+
+  isHolidays(date: Date) : boolean {
+    const day = date.getDate();
+    const month = date.getMonth();
+    const year = date.getFullYear();
+
+    const holidays = this.getHolidays(year);
+
+    // some() va itérer sur tous les éléments de holidays et appliquer 
+    // la fonction fléchée fournie à chaque élément (holiday)
+    return holidays.some(holiday =>
+      holiday.getFullYear() === year &&
+      holiday.getMonth() === month &&
+      holiday.getDate() === day);
   }
 
 }
