259822e0cd8998fc6b9d39532504e9f45148f3b9
Ben Carra <benedictecarra@gmail.com>
2024-03-08 17:31:50 +0100

    modif de la route listTaches avec ajout du numéro de semaine

--- front/src/app/shared/component/navbar/navbar.component.ts
+++ front/src/app/shared/component/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -9,6 +9,26 @@ import { RouterLink } from '@angular/router';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+
+  date : Date = new Date();
+  weekNumber : number = 1 ;
+
+  ngOnInit(): void {
+    this.weekNumber = this.getWeekNumber(this.date);    
+  }
+
+  // Méthode qui calcule le numéro de semaine par rapport à une date donnée  
+  getWeekNumber(d: Date): number {
+    // Copie de la date pour éviter de modifier l'original
+    d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+    // Définir au dimanche le plus proche
+    d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
+    // Date de début de l'année
+    const yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
+    // Calcul de la différence de jours et division par 7 pour obtenir le numéro de semaine
+    const weekNo = Math.ceil(( (d.getTime() - yearStart.getTime()) / 86400000 + 1)/7);
+    return weekNo;
+  }
 
 }
