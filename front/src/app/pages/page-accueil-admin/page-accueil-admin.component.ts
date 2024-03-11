3ae1ed3af74f5ca23032a8637e23a9482900f052
Adrien Meyrand <adrien.meyrand@gmail.com>
2024-03-11 08:45:08 +0100

    Ajout liens vers formulaires création sur page accueil admin

--- front/src/app/pages/page-accueil-admin/page-accueil-admin.component.ts
+++ front/src/app/pages/page-accueil-admin/page-accueil-admin.component.ts
@@ -5,13 +5,14 @@ import { FooterComponent } from "../../shared/component/footer/footer.component"
 import { UtilisateurListComponent } from "../utilisateur-list/utilisateur-list.component";
 import { ClientListComponent } from "../client-list/client-list.component";
 import { EquipeListComponent } from "../equipe-list/equipe-list.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-page-accueil-admin',
     standalone: true,
     templateUrl: './page-accueil-admin.component.html',
     styleUrl: './page-accueil-admin.component.scss',
-    imports: [HeaderComponent, FooterComponent, UtilisateurListComponent, ClientListComponent, EquipeListComponent]
+    imports: [RouterLink ,HeaderComponent, FooterComponent, UtilisateurListComponent, ClientListComponent, EquipeListComponent]
 })
 export class PageAccueilAdminComponent {
 
