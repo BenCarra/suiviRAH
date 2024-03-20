83fed76127d14c903fb18d32ab20c27a9e291722
Ben Carra <benedictecarra@gmail.com>
2024-03-20 17:54:10 +0100

    Modification de la page de connexion avec la maquette d'Angélique

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,14 +1,36 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationStart, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./shared/component/navbar/navbar.component";
+import { CommonModule } from '@angular/common';
+
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, NavbarComponent]
+    imports: [RouterOutlet, 
+    NavbarComponent,
+    CommonModule]
 })
-export class AppComponent {
-  title = 'front';
+export class AppComponent implements OnInit {
+  // Je suppose que la navbar est toujours visible
+  showNavbar: boolean = true;
+
+  constructor(private router:Router) {
+  }
+
+  ngOnInit(): void {
+    // Écoute les changements de route pour déterminer 
+    // si la barre de navigation doit être affichée
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        // Si l'URL est celle de la page d'accueil 
+        // je masque la barre de navigation
+        this.showNavbar = event.url !== '/';
+      }
+    })
+  }
+
+  title = 'Super front !';
 }
